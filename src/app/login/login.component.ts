73de031ddc3962d48444cdeb8a95a88d6a98043c
Jason Richardson <jrichardson@skillstorm.com>
2021-10-27 16:19:41 -0500

    login component request

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../model/user.model';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -9,52 +10,29 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  form = this.formBuilder.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required]
-});
-  loading = false;
-  submitted = false;
-
+  users: User = new User('', '', '');
+  
+ 
   constructor(      
-      private formBuilder: FormBuilder,
-      private route: ActivatedRoute,
       private router: Router,
       private userService: UserService,
-      //private alertService: AlertService
+
   ) { }
 
   ngOnInit() {
       
   }
 
-  get f() { return this.form.controls; }
+
 
     onSubmit() {
-        this.submitted = true;
-
-        // reset alerts on submit
-        //this.alertService.clear();
-
-        // stop here if form is invalid
-        if (this.form.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        // this.userService.login(this.f.username.value, this.f.password.value)
-        //     .pipe(first())
-        //     .subscribe({
-        //         next: () => {
-        //             // get return url from query parameters or default to home page
-        //             const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-        //             this.router.navigateByUrl(returnUrl);
-        //         },
-        //         error: (err: any) => {
-        //           console.log(err);
-        //             //this.alertService.error(error);
-        //             this.loading = false;
-        //         }
-        //     });
+        
+        this.userService.getUser(this.users).subscribe(data => {
+          console.log("Checked User Id: " + data);
+          let route = this.router.config.find(r => r.path === 'accountsummary');
+          if (route) {
+            this.router.navigate(['/accountsummary']);
+          }
+        });
     }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,10 +13,16 @@ import { environment } from 'src/environments/environment.prod';
 export class UserService {
 
   url = `${environment.apiURL}api/Users`;
+  login_url = `${environment.apiURL}api/Users/Login`;
   constructor(private router: Router, private http: HttpClient) {
   }
 
   addUser(user: User): Observable<number> {
     return this.http.post<number>(this.url, user);
   }
+
+  getUser(user: User): Observable<User> {
+    return this.http.post<User>(this.login_url, user);
+  }
+
 }

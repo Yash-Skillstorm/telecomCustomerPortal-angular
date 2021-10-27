025f7c1fae41c9d9b413426a716e80f19954872c
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-10-26 21:49:40 -0500

    remove unwanted code

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,32 +13,10 @@ import { environment } from 'src/environments/environment.prod';
 export class UserService {
 
   url = `${environment.apiURL}api/Users`;
-  constructor(private router: Router, private http: HttpClient) { 
+  constructor(private router: Router, private http: HttpClient) {
   }
 
-
-    addUser(user: User): Observable<number> {      
-      return this.http.post<number>(this.url, user);
-    }
+  addUser(user: User): Observable<number> {
+    return this.http.post<number>(this.url, user);
   }
-//the HTTP post request
-
-      //this.userSubject = new BehaviorSubject<User>(JSON.parse());
-        //this.user = this.userSubject.asObservable();
-     
-
-    //  login(username : any, password: any) {
-    //   return this.http.post<User>(this.url, { username, password })
-    //       .pipe(map(user => {
-    //           // store user details and jwt token in local storage to keep user logged in between page refreshes
-    //           localStorage.setItem('user', JSON.stringify(user));
-    //           this.userSubject.next(user);
-    //           return user;
-    //       }));
-  
- 
-  // logout() {
-  //   // remove user from local storage and set current user to null
-  //   localStorage.removeItem('user');
-  //   this.userSubject.next(null);
-  //   this.router.navigate(['/account/login']);
+}
